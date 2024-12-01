a58343631240400067bccd3f8c67baf1d6111fc6
Bart de Boer <bart.de.boer@divverence.com>
2024-12-01 08:06:51 +0100

    The Template

---
+++ day__/clone.ps1
@@ -0,0 +1,12 @@
+if ($args.count -eq 0) { write-host "Pass number please"; return }
+$X=$args[0]
+Remove-Item "../day$X" -force -recurse
+mkdir "../day$X"
+Copy-Item *.cs* "../day$X"
+Set-Location "../day$X"
+fart -f "*.*" __ $X
+fart "*.cs" __ $x
+dotnet build
+Set-Location "../answers"
+dotnet add reference "../day$X"
+Set-Location "../day__"
