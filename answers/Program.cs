eef6a4439c45631118c652a6f7937c03f9762b90
Bart de Boer <bart.de.boer@divverence.com>
2024-12-01 08:31:41 +0100

    The Template - extended

--- answers/Program.cs
+++ answers/Program.cs
@@ -1,26 +1,27 @@
 ﻿Console.WriteLine($"Day  1 Part 1: {day1.D1P1.Part1Answer(day1.Input.RawInput)}");
 Console.WriteLine($"Day  1 Part 2: {day1.D1P2.Part2Answer(day1.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  2 Part 1: {day2.D2P1.Part1Answer(day2.Input.RawInput)}");
 Console.WriteLine($"Day  2 Part 2: {day2.D2P2.Part2Answer(day2.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  3 Part 1: {day3.D3P1.Part1Answer(day3.Input.RawInput)}");
 Console.WriteLine($"Day  3 Part 2: {day3.D3P2.Part2Answer(day3.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  4 Part 1: {day4.D4P1.Part1Answer(day4.Input.RawInput)}");
 Console.WriteLine($"Day  4 Part 2: {day4.D4P2.Part2Answer(day4.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  5 Part 1: {day5.D5P1.Part1Answer(day5.Input.RawInput)}");
 Console.WriteLine($"Day  5 Part 2: {day5.D5P2.Part2Answer(day5.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  6 Part 1: {day6.D6P1.Part1Answer(day6.Input.RawInput)}");
 Console.WriteLine($"Day  6 Part 2: {day6.D6P2.Part2Answer(day6.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  7 Part 1: {day7.D7P1.Part1Answer(day7.Input.RawInput)}");
 Console.WriteLine($"Day  7 Part 2: {day7.D7P2.Part2Answer(day7.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  8 Part 1: {day8.D8P1.Part1Answer(day8.Input.RawInput)}");
 Console.WriteLine($"Day  8 Part 2: {day8.D8P2.Part2Answer(day8.Input.RawInput)}");
-
+/*
 Console.WriteLine($"Day  9 Part 1: {day9.D9P1.Part1Answer(day9.Input.RawInput)}");
 Console.WriteLine($"Day  9 Part 2: {day9.D9P2.Part2Answer(day9.Input.RawInput)}");
+*/
