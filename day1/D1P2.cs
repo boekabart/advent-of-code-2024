447c655923f7b17660f967b42240370922f47e4c
Bart de Boer <bart.de.boer@divverence.com>
2024-12-01 08:42:07 +0100

    Day 1 Part 2

--- day1/D1P2.cs
+++ day1/D1P2.cs
@@ -2,6 +2,12 @@
 
 public static class D1P2
 {
-    public static object Part2Answer(this string input) =>
-        new NotImplementedException();
+    public static object Part2Answer(this string input)
+    {
+        var lists = input.ParseThings().ToList();
+        var list1 = lists.Select(t => t.Number1).Order().ToList();
+        var list2 = lists.Select(t => t.Number2).Order().ToList();
+        return list1.Select(n1 => list2.Count(n2 => n2==n1) * n1).Sum();
+    }
+
 }
--- day1/D1P2Tests.cs
+++ day1/D1P2Tests.cs
@@ -5,19 +5,19 @@ namespace day1;
 
 public class D1P2Tests
 {
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 31;
         Input.ExampleInput
             .Part2Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 17191599;
         Input.RawInput
             .Part2Answer()
             .Should().Be(expected);
