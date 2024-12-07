d7b05818133d9214597207de9416f41d771cd176
Bart de Boer <bart.de.boer@divverence.com>
2024-12-07 06:56:02 +0100

    Day 7 Part 2

--- day7/D7P2.cs
+++ day7/D7P2.cs
@@ -3,5 +3,35 @@
 public static class D7P2
 {
     public static object Part2Answer(this string input) =>
-        new NotImplementedException();
+        input.ParseThings().GetResult2();
+    internal static long GetResult2(this IEnumerable<Thing> things) => things.Where(AsResult2).Aggregate((long)0, (l, thing) => thing.Answer + l);
+
+    internal static bool AsResult2(this Thing thing)
+    {
+        if (thing.Operands.Count == 1) return thing.Operands[0] == thing.Answer;
+        return thing.Plussed().AsResult2()
+               || thing.Multiplied().AsResult2()
+        || thing.Orred().AsResult2();
+    }
+
+    internal static Thing Orred(this Thing thing)
+    {
+        return thing.Modified((a, b) => long.Parse(a.ToString() + b));
+    }
+    internal static Thing Plussed(this Thing thing)
+    {
+        return thing.Modified((a, b) => a+b);
+    }
+    internal static Thing Multiplied(this Thing thing)
+    {
+        return thing.Modified((a, b) => a*b);
+    }
+
+    internal static Thing Modified(this Thing thing, Func<long, long, long> op)
+    {
+        var first = thing.Operands[0];
+        var second = thing.Operands[1];
+        var newFirst = op(first, second);
+        return thing with { Operands = thing.Operands.Skip(2).Prepend(newFirst).ToList() };
+    }
 }
--- day7/D7P2Tests.cs
+++ day7/D7P2Tests.cs
@@ -5,19 +5,33 @@ namespace day7;
 
 public class D7P2Tests
 {
-    [Fact(Skip = "ToDo")]
+    [Fact]
+    internal static void OrTests()
+    {
+        var orr = new Thing(12345, [12, 345]).Orred();
+        orr.Answer.Should().Be(12345);
+        orr.Operands.Should().BeEquivalentTo([12345]);
+    }
+
+    [Fact]
+    internal static void TestCases()
+    {
+        "7290: 6 8 6 15".TryParseAsThing().AsResult2().Should().BeTrue();
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 11387;
         Input.ExampleInput
             .Part2Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 92148721834692L;
         Input.RawInput
             .Part2Answer()
             .Should().Be(expected);
