bd84e1f335da2ffeee81ff4d959a97cc7cdb2106
Bart de Boer <bart.de.boer@divverence.com>
2024-12-09 10:23:19 +0100

    Day 8 Part 1

--- day8/D8P1.cs
+++ day8/D8P1.cs
@@ -2,24 +2,92 @@
 
 namespace day8;
 
-internal record Thing(bool Data);
+internal record Map(char[][] Grid);
+
+internal record Pos(int X, int Y);
 
 public static class D8P1
 {
-    public static object Part1Answer(this string input) =>
-        new NotImplementedException();
+    public static int Part1Answer(this string input) 
+    {
+        return input.ParseMap()
+            .GetAllBuiken()
+            .Count();
+    }
 
-    internal static IEnumerable<Thing> ParseThings(this string input) =>
+    internal static Map ParseMap(this string input) =>
         input
             .Lines()
             .Select(TryParseAsThing)
-            .OfType<Thing>();
+            .OfType<string>()
+            .AsMap();
 
-    internal static Thing? TryParseAsThing(this string line)
+    internal static string? TryParseAsThing(this string line)
     {
-        return null;
+        return string.IsNullOrWhiteSpace(line) ? null : line.Trim();
     }
 
-    internal static int GetResult(this IEnumerable<Thing> things) => things.Select(AsResult).Sum();
-    internal static int AsResult(this Thing thing) => 0;
+    //internal static int GetResult(this Map things) => things.Select(AsResult).Sum();
+
+    internal static Map AsMap(this IEnumerable<string> lines)
+    {
+        var grid = lines.Select(line => line.ToArray()).ToArray();
+        return new Map(grid);
+    }
+
+    internal static IEnumerable<(char Freq, Pos Pos)> GetAntennas(this Map map)
+    {
+        for (int y = 0; y < map.Grid.Length; y++)
+        for (int x = 0; x < map.Grid[y].Length; x++)
+        {
+            var kar = map.Grid[y][x];
+            if (IsFreq(kar))
+                yield return (kar, new(x, y));
+        }
+    }
+
+    internal static IEnumerable<Pos> GetAllBuiken(this Map map)
+    {
+        var sizeY = map.Grid.Length;
+        var sizeX = map.Grid[0].Length;
+
+        return map.GetAntennas()
+                .GetAllBuiken()
+                .Where(buik => buik.X >= 0 && buik.X < sizeX && buik.Y >= 0 && buik.Y < sizeY)
+            ;
+    }
+
+    internal static IEnumerable<Pos> GetAllBuiken(this IEnumerable<(char Freq, Pos Pos)> antennas)
+    {
+        return antennas.SplitByFrequency().SelectMany(
+                freq => freq.GetAllPairs().SelectMany(pair => pair.GetBuiken()))
+            .Distinct();
+    }
+
+
+    internal static IEnumerable<List<Pos>> SplitByFrequency(this IEnumerable<(char Freq, Pos Pos)> antennas)
+    {
+        return antennas
+            .GroupBy( a => a.Freq ).Select(gr => gr.Select(a => a.Pos).ToList());
+    }
+
+    internal static IEnumerable<(Pos Pos1, Pos Pos2)> GetAllPairs(this IList<Pos> positions)
+    {
+        for (var one = 0; one < positions.Count -1; one++)
+        for (var two = one+1; two < positions.Count; two++)
+            yield return (positions[one], positions[two]);
+    }
+
+    internal static IEnumerable<Pos> GetBuiken(this (Pos Pos1, Pos Pos2) pair)
+    {
+        var dX = pair.Pos2.X - pair.Pos1.X;
+        var dY = pair.Pos2.Y - pair.Pos1.Y;
+        yield return new(pair.Pos2.X + dX, pair.Pos2.Y + dY);
+        yield return new(pair.Pos1.X - dX, pair.Pos1.Y - dY);
+    }
+
+    internal static bool  IsFreq(this char kar)
+    {
+        return kar is >= '0' and <= '9' or >= 'A' and <= 'Z' or >= 'a' and <= 'z';
+    }
 }
--- day8/D8P1Tests.cs
+++ day8/D8P1Tests.cs
@@ -5,34 +5,94 @@ namespace day8;
 
 public static class D8P1Tests
 {
-    [InlineData("",null)]
-    [Theory]
-    internal static void ParseInputLineTest(string line, Thing? expectedThing)
-    {
-        var actualThing = line.TryParseAsThing();
-        actualThing.Should().Be(expectedThing);
-    }
-
     [Fact]
     internal static void ParseInputTest()
     {
-        var things = Input.ExampleInput.ParseThings().ToArray();
-        things.Should().HaveCount(0);
+        var map = Input.ExampleInput.ParseMap();
+        map.Grid.Should().HaveCount(12);
+        map.Grid.Should().AllSatisfy(line => line.Should().HaveCount(12));
     }
 
-    [Fact(Skip="ToDo")]
+    [Fact]
+    internal static void AntennaTest()
+    {
+        var map = Input.ExampleInput.ParseMap();
+        var actual = map.GetAntennas().ToList();
+        actual.Should().HaveCount(7);
+    }
+
+    [Fact]
+    internal static void BuikenTest1()
+    {
+        const string q = @"
+..........
+...#......
+..........
+....a.....
+..........
+.....a....
+..........
+......#...
+..........
+..........";
+        var map = q.ParseMap();
+        var actual = map.GetAllBuiken();
+        actual.Should().HaveCount(2);
+    }
+
+    [Fact]
+    internal static void BuikenTest2a()
+    {
+        const string q = @"
+..........
+...#......
+#.........
+....a.....
+........a.
+.....a....
+..#.......
+......#...
+..........
+..........";
+        var map = q.ParseMap();
+        map.GetAntennas().Should().HaveCount(3);
+        map.GetAntennas().Select(a => a.Pos).ToList().GetAllPairs().Should().HaveCount(3);
+        var actual = map.GetAllBuiken();
+        actual.Should().HaveCount(4);
+    }
+
+    [Fact]
+    internal static void BuikenTest2()
+    {
+        const string q = @"
+..........
+...#......
+#.........
+....a.....
+........a.
+.....a....
+..#.......
+......#...
+..........
+..........";
+        var map = q.ParseMap();
+        var actual = map.GetAllBuiken();
+        actual.Should().HaveCount(4);
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 14;
         Input.ExampleInput
             .Part1Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 341;
         Input.RawInput
             .Part1Answer()
             .Should().Be(expected);
