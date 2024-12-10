48b6c875b4eb4f02631662d0be0aeccaf29b369f
Bart de Boer <bart.de.boer@divverence.com>
2024-12-10 21:42:11 +0100

    Day 10 Part 1

--- day10/D10P1.cs
+++ day10/D10P1.cs
@@ -1,4 +1,5 @@
-﻿using shared;
+﻿using System.Collections;
+using shared;
 
 namespace day10;
 
@@ -7,19 +8,50 @@ internal record Thing(bool Data);
 public static class D10P1
 {
     public static object Part1Answer(this string input) =>
-        new NotImplementedException();
+        input.ParseMap().Go();
 
-    internal static IEnumerable<Thing> ParseThings(this string input) =>
-        input
-            .Lines()
-            .Select(TryParseAsThing)
-            .OfType<Thing>();
-
-    internal static Thing? TryParseAsThing(this string line)
+    internal static IEnumerable<Pos> FindAll(this Map map, char q)
     {
-        return null;
+        for (int y =0; y<map.Grid.Length; y++)
+        for (int x = 0; x < map.Grid[y].Length; x++)
+            if (map.Grid[y][x]==q)
+                yield return new Pos(x, y);
     }
 
-    internal static int GetResult(this IEnumerable<Thing> things) => things.Select(AsResult).Sum();
-    internal static int AsResult(this Thing thing) => 0;
+    internal static int Go(this Map map)
+    {
+        var mapOfReachableNines = map.Convert(kar => new HashSet<Pos>());
+        Queue<Pos> todo = [];
+        foreach (var nine in map.FindAll('9'))
+        {
+            mapOfReachableNines.Get(nine).Add(nine);
+            todo.Enqueue(nine);
+        }
+
+        while (todo.Count > 0)
+        {
+            var pos = todo.Dequeue();
+            var kar = map.Grid[pos.Y][pos.X];
+            if (kar == '0')
+                continue;
+
+            var lowerKar = (char)(kar - 1);
+            var ninePositionsToCopy = mapOfReachableNines.Get(pos);
+            foreach (var neighbour in pos.FourAround())
+            {
+                var nbKar = map.TryGet(neighbour);
+                if (nbKar != lowerKar)
+                    continue;
+                int added = 0;
+                foreach (var ninePos in ninePositionsToCopy)
+                    if (mapOfReachableNines.Get(neighbour).Add(ninePos))
+                        added++;
+                if (added > 0)
+                    todo.Enqueue(neighbour);
+            }
+        }
+
+        return map.FindAll('0').SelectMany(mapOfReachableNines.Get).Count();
+    }
+
 }
--- day10/D10P1Tests.cs
+++ day10/D10P1Tests.cs
@@ -1,38 +1,40 @@
 ﻿using FluentAssertions;
+using shared;
 using Xunit;
 
 namespace day10;
 
 public static class D10P1Tests
 {
-    [InlineData("",null)]
-    [Theory]
-    internal static void ParseInputLineTest(string line, Thing? expectedThing)
-    {
-        var actualThing = line.TryParseAsThing();
-        actualThing.Should().Be(expectedThing);
-    }
-
     [Fact]
     internal static void ParseInputTest()
     {
-        var things = Input.ExampleInput.ParseThings().ToArray();
-        things.Should().HaveCount(0);
+        const string ex1 = @"
+0123
+1234
+8765
+9876";
+        var map = ex1.ParseMap();
+
+        map.FindAll('0').Should().BeEquivalentTo([new Pos(0, 0)]);
+        map.FindAll('9').Should().BeEquivalentTo([new Pos(0, 3)]);
+
+        map.Go().Should().Be(1);
     }
 
-    [Fact(Skip="ToDo")]
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 36;
         Input.ExampleInput
             .Part1Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 538;
         Input.RawInput
             .Part1Answer()
             .Should().Be(expected);
