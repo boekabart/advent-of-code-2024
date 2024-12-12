ef470b25698fbf782df7ab0528a99821974510eb
Bart de Boer <bart.de.boer@divverence.com>
2024-12-12 06:46:14 +0100

    Day 12 Part 2

--- day12/D12P2.cs
+++ day12/D12P2.cs
@@ -1,7 +1,68 @@
-﻿namespace day12;
+﻿using shared;
+
+namespace day12;
 
 public static class D12P2
 {
     public static object Part2Answer(this string input) =>
-        new NotImplementedException();
+        input.ParseMap().GetResult2();
+
+
+    internal static int GetResult2(this Map map)
+    {
+        var regions = map.GetRegions();
+        return regions.Sum(CalcFenceCost2);
+
+    }
+
+    internal static int CalcFenceCost2(this Region region)
+    {
+        var area = region.Locations.Count;
+        var sides = region.CountSides();
+        return area * sides;
+    }
+
+    internal static int CountSides(this Region region)
+    {
+        return region.CountSides(p => p.Up())
+               + region.CountSides(p => p.Down())
+               + region.CountSides(p => p.Left())
+               + region.CountSides(p => p.Right());
+    }
+
+    internal static int CountSides(this Region region, Func<Pos, Pos> dirToCheck)
+    {
+        var positionsOutsideRegionInThisDir = region
+            .Locations
+            .Select(dirToCheck)
+            .Where(up => !region.Locations.Contains(up))
+            .ToHashSet();
+        return positionsOutsideRegionInThisDir.CountContiguousGroups();
+    }
+
+    internal static int CountContiguousGroups(this HashSet<Pos> allPositions)
+    {
+        HashSet<Pos> alreadyGrouped = [];
+        int contiguousGroups = 0;
+        foreach (var pos in allPositions)
+        {
+            if (alreadyGrouped.Contains(pos))
+                continue;
+
+            contiguousGroups++;
+
+            Stack<Pos> toCheck = [];
+            toCheck.Push(pos);
+            while (toCheck.Count > 0)
+            {
+                var p = toCheck.Pop();
+                if (!alreadyGrouped.Add(p))
+                    continue;
+                foreach (var neighbour in p.FourAround().Where(allPositions.Contains))
+                    toCheck.Push(neighbour);
+            }
+        }
+
+        return contiguousGroups;
+    }
 }
--- day12/D12P2Tests.cs
+++ day12/D12P2Tests.cs
@@ -1,23 +1,51 @@
 ﻿using FluentAssertions;
+using shared;
 using Xunit;
 
 namespace day12;
 
 public class D12P2Tests
 {
-    [Fact(Skip = "ToDo")]
+    [Fact]
+    internal static void CalcFenceCostTest()
+    {
+        var map = @"
+AAAA
+BBCD
+BBCC
+EEEC".ParseMap();
+        var regionA = map.FindRegion(new Pos(0, 0));
+        regionA.CountSides().Should().Be(4);
+        var regionC = map.FindRegion(new Pos(2, 1));
+        regionC.CountSides().Should().Be(8);
+    }
+
+    [Fact]
+    internal static void CalcFenceCostTest2()
+    {
+        var map = @"
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE".ParseMap();
+        var region = map.FindRegion(new Pos(0, 0));
+        region.CountSides().Should().Be(12);
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 1206;
         Input.ExampleInput
             .Part2Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 865662;
         Input.RawInput
             .Part2Answer()
             .Should().Be(expected);
