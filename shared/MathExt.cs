39e99f19ebacae695c4539af1345b9632c0ce687
Bart de Boer <bart.de.boer@divverence.com>
2024-12-13 13:41:05 +0100

    Add MathExt (turned out not needed for Day 13)

---
+++ shared/MathExt.cs
@@ -0,0 +1,42 @@
+﻿namespace shared;
+
+public static class MathExt
+{
+    public static int GGD(this int a, int b)
+    {
+        // Aannames: 
+        // * a en b zijn ≥ 0 
+        // * Als een van beide 0 is, geef het andere getal terug
+        // * Beide 0 komt niet voor
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        // Euclidisch algoritme
+        while (b != 0)
+        {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+    public static long GGD(this long a, long b)
+    {
+        // Aannames: 
+        // * a en b zijn ≥ 0 
+        // * Als een van beide 0 is, geef het andere getal terug
+        // * Beide 0 komt niet voor
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        // Euclidisch algoritme
+        while (b != 0)
+        {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+}
