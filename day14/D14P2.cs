cb2c978b098bb164ad282fc2b12f50b63bfdb6c5
Bart de Boer <bart.de.boer@divverence.com>
2024-12-14 09:30:38 +0100

    Day 14 Part 2

--- day14/D14P2.cs
+++ day14/D14P2.cs
@@ -1,7 +1,36 @@
-﻿namespace day14;
+﻿using shared;
+
+namespace day14;
 
 public static class D14P2
 {
-    public static object Part2Answer(this string input) =>
-        new NotImplementedException();
+    public static object Part2Answer(this string input)
+    {
+        var mapSize = new Pos(101, 103);
+        int seconds = 0;
+
+        var robots = input.ParseThings().ToArray();
+
+        while (seconds < mapSize.X * mapSize.Y)
+        {
+            var positions = robots.Select(r => r.AsResult(seconds, mapSize)).ToHashSet();
+            var positionsThatMightBeAPeakBecauseTheOneAboveItDoesntExist = positions.Where(p => !positions.Contains(p.Up()));
+            var positionsWithAllPositionsToTheBottomLeftAndBottomRightExisting = positionsThatMightBeAPeakBecauseTheOneAboveItDoesntExist
+                .Where( p => p.FiveToBottomLeftAndBottomRight().All(positions.Contains));
+            if (positionsWithAllPositionsToTheBottomLeftAndBottomRightExisting.Any())
+            {
+                return seconds;
+            }
+
+            seconds++;
+        }
+
+        return -1;
+    }
+
+    internal static IEnumerable<Pos> FiveToBottomLeftAndBottomRight(this Pos pos) =>
+        pos.InDir(new Pos(-1, 1), 4).Concat(pos.InDir(new Pos(1, 1), 4));
+
+
+    internal static IEnumerable<Pos> InDir(this Pos pos, Pos dir, int count) => Enumerable.Range(1, count).Select(m => pos.Add(dir.Times(m)));
 }
--- day14/D14P2Tests.cs
+++ day14/D14P2Tests.cs
@@ -1,23 +1,15 @@
 ﻿using FluentAssertions;
+using shared;
 using Xunit;
 
 namespace day14;
 
 public class D14P2Tests
 {
-    [Fact(Skip = "ToDo")]
-    internal static void AcceptanceTest()
-    {
-        var expected = 42;
-        Input.ExampleInput
-            .Part2Answer()
-            .Should().Be(expected);
-    }
-
-    [Fact(Skip = "ToDo")]
+    [Fact]
     internal static void RegressionTest()
     {
-        var expected = 42;
+        var expected = 8149;
         Input.RawInput
             .Part2Answer()
             .Should().Be(expected);
