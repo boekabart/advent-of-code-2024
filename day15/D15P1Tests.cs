30b9a890ce3d609caa2f2e37f3ed380987004924
Bart de Boer <bart.de.boer@divverence.com>
2024-12-15 16:50:53 +0100

    Prep day 15

---
+++ day15/D15P1Tests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace day15;
+
+public static class D15P1Tests
+{
+    [Fact]
+    internal static void ParseInputLineTest()
+    {
+        var actualThing = "".TryParseAsThing();
+        actualThing.Should().Be(null);
+    }
+
+    [Fact]
+    internal static void ParseInputTest()
+    {
+        var things = Input.ExampleInput.ParseThings().ToArray();
+        things.Should().HaveCount(0);
+    }
+
+    [Fact(Skip="ToDo")]
+    internal static void AcceptanceTest()
+    {
+        var expected = 42;
+        Input.ExampleInput
+            .Part1Answer()
+            .Should().Be(expected);
+    }
+
+    [Fact(Skip = "ToDo")]
+    internal static void RegressionTest()
+    {
+        var expected = 42;
+        Input.RawInput
+            .Part1Answer()
+            .Should().Be(expected);
+    }
+}
---
+++ day15/D15P2.cs
@@ -0,0 +1,7 @@
+﻿namespace day15;
+
+public static class D15P2
+{
+    public static object Part2Answer(this string input) =>
+        new NotImplementedException();
+}
