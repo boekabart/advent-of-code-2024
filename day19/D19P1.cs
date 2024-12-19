695326c1ec8d11922d4e3c17ed4f9a5c7623cb3f
Bart de Boer <bart.de.boer@divverence.com>
2024-12-19 20:20:46 +0100

    Day 19 Part 1 too slow

---
+++ day19/D19P1.cs
@@ -0,0 +1,52 @@
+﻿using shared;
+
+namespace day19;
+
+internal record Towel(string Stripes);
+
+internal record Pattern(string Stripes);
+
+public static class D19P1
+{
+    public static object Part1Answer(this string input)
+    {
+        var towels = input.ParseTowels().ToList();
+        return input.ParsePatterns().AsParallel().Count(p => p.CanBeMade(towels));
+    }
+
+    internal static bool CanBeMade(this Pattern pattern, List<Towel> towels)
+    {
+        foreach (var t in towels)
+        {
+            if (pattern.Stripes.StartsWith(t.Stripes))
+            {
+                if (pattern.Stripes.Length == t.Stripes.Length)
+                    return true;
+                var subPattern = new Pattern(pattern.Stripes.Substring(t.Stripes.Length));
+                if (subPattern.CanBeMade(towels))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    internal static IEnumerable<Pattern> ParsePatterns(this string input) =>
+        input
+            .NotEmptyTrimmedLines()
+            .Where(l => !l.Contains(','))
+            .Select(l => new Pattern(l));
+
+    internal static IEnumerable<Towel> ParseTowels(this string input) =>
+        input
+            .Lines()
+            .SelectMany(TryParseAsTowels);
+
+    internal static IEnumerable<Towel> TryParseAsTowels(this string line)
+    {
+        var split = line.Split([',', ' '], StringSplitOptions.RemoveEmptyEntries);
+        return split.Length > 1
+            ? split.Select(s => new Towel(s))
+            :[];
+    }
+
+}
---
+++ day19/D19P1Tests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace day19;
+
+public static class D19P1Tests
+{
+
+    [Fact]
+    internal static void ParseInputTest()
+    {
+        var towels = Input.ExampleInput.ParseTowels().ToArray();
+        towels.Should().HaveCount(8);
+        towels[0].Stripes.Should().Be("r");
+
+        var patt = Input.ExampleInput.ParsePatterns().ToArray();
+        patt.Should().HaveCount(8);
+    }
+
+    [Fact]
+    internal static void CanBeMadeTest()
+    {
+        var towels = Input.ExampleInput.ParseTowels().ToList();
+        towels.Should().HaveCount(8);
+
+        new Pattern(towels.Last().Stripes).CanBeMade(towels).Should().BeTrue();
+
+        new Pattern("gekkie").CanBeMade(towels).Should().BeFalse();
+
+        new Pattern("brwrr").CanBeMade(towels).Should().BeTrue();
+        new Pattern("rrbgbr").CanBeMade(towels).Should().BeTrue();
+        new Pattern("ubwu").CanBeMade(towels).Should().BeFalse();
+        new Pattern("bwurrg").CanBeMade(towels).Should().BeTrue();
+    }
+
+    [Fact]
+    internal static void AcceptanceTest()
+    {
+        var expected = 6;
+        Input.ExampleInput
+            .Part1Answer()
+            .Should().Be(expected);
+    }
+
+    [Fact(Skip = "ToDo")]
+    internal static void RegressionTest()
+    {
+        var expected = 42;
+        Input.RawInput
+            .Part1Answer()
+            .Should().Be(expected);
+    }
+}
