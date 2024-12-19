89ae85a69b8d80c2646e32e50c23826721e6f234
Bart de Boer <bart.de.boer@divverence.com>
2024-12-19 22:22:59 +0100

    blergh

--- day19/D19P2.cs
+++ day19/D19P2.cs
@@ -2,6 +2,79 @@
 
 public static class D19P2
 {
-    public static object Part2Answer(this string input) =>
-        new NotImplementedException();
+    public static object Part2Answer(this string input)
+    {
+        var towels = input.ParseTowels().ToList();
+        return input.ParsePatterns()
+            .OrderBy(p => p.Stripes.Length)
+            .CanBeMade2(towels).Sum(boel => boel);
+    }
+
+    internal static IEnumerable<int> CanBeMade2(this IEnumerable<Pattern> patterns, IEnumerable<Towel> towels)
+    {
+        Dictionary<string, int> yes = towels.ToDictionary();
+        HashSet<string> no = [];
+        return patterns.CanBeMade(yes, no);
+    }
+
+    internal static Dictionary<string, int> ToDictionary(this IEnumerable<Towel> towels)
+    {
+        var retval = new Dictionary<string, int>();
+        foreach (var t in towels)
+        {
+            var p = new Pattern(t.Stripes);
+            var n = p.CanBeMade2(towels.Where(tw => tw != t));
+            retval[t.Stripes] = n + 1;
+        }
+
+        return retval;
+    }
+
+    internal static int CanBeMade2(this Pattern pattern, IEnumerable<Towel> towels)
+    {
+        Dictionary<string, int> yes = towels.ToDictionary();
+        HashSet<string> no = [];
+        return pattern.CanBeMade(yes, no);
+    }
+
+    internal static IEnumerable<int> CanBeMade(this IEnumerable<Pattern> pattern, Dictionary<string,int> yes,
+        HashSet<string> no)
+    {
+        return pattern.Select(p => p.CanBeMade(yes, no));
+    }
+
+    internal static int CanBeMade(this Pattern pattern, Dictionary<string,int> yes, HashSet<string> no)
+    {
+        if (yes.TryGetValue(pattern.Stripes, out var k)) return k;
+        if (no.Contains(pattern.Stripes)) return 0;
+        int n = 0;
+        HashSet<string> alreadyChecked = [];
+        var toCheck = yes.Keys.OrderBy(kk => kk.Length).ToList();
+        while (true)
+        {
+            if (toCheck.Count == 0) break;
+
+            foreach (var t in toCheck)
+            {
+                if (pattern.Stripes.StartsWith(t))
+                {
+                    var subPattern = new Pattern(pattern.Stripes.Substring(t.Length));
+                    n += subPattern.CanBeMade(yes, no) * yes[t];
+                }
+
+                alreadyChecked.Add(t);
+            }
+
+            toCheck = yes.Keys.Except(alreadyChecked).ToList();
+        }
+
+        if (n > 0)
+        {
+            yes.Add(pattern.Stripes, n);
+            return n;
+        }
+
+        no.Add(pattern.Stripes);
+        return 0;
+    }
 }
--- day19/D19P2Tests.cs
+++ day19/D19P2Tests.cs
@@ -5,16 +5,41 @@ namespace day19;
 
 public class D19P2Tests
 {
-    [Fact(Skip = "ToDo")]
+    [Fact]
+    internal static void Testjes()
+    {
+        var towels = Input.ExampleInput.ParseTowels().ToList();
+        towels.Should().HaveCount(8);
+        /*
+        string.Join(',',
+                Input.ExampleInput.ParsePatterns()
+                    .CanBeMade2(towels)).Should()
+            .NotBe("2,1,4,6,0,1,2,0");
+
+        new Pattern("b").CanBeMade2(towels).Should().Be(1);
+        new Pattern("br").CanBeMade2(towels).Should().Be(2);
+        new Pattern("gb").CanBeMade2(towels).Should().Be(2);
+        */
+        new Pattern("brwrr").CanBeMade2(towels).Should().Be(2);
+        new Pattern("bggr").CanBeMade2(towels).Should().Be(1);
+        new Pattern("gbbr").CanBeMade2(towels).Should().Be(4);
+        new Pattern("rrbgbr").CanBeMade2(towels).Should().Be(6);
+        new Pattern("bwurrg").CanBeMade2(towels).Should().Be(1);
+        new Pattern("brgr").CanBeMade2(towels).Should().Be(2);
+        new Pattern("ubwu").CanBeMade2(towels).Should().Be(0);
+        new Pattern("bbrgwb").CanBeMade2(towels).Should().Be(0);
+    }
+
+    [Fact]
     internal static void AcceptanceTest()
     {
-        var expected = 42;
+        var expected = 16;
         Input.ExampleInput
             .Part2Answer()
             .Should().Be(expected);
     }
 
-    [Fact(Skip = "ToDo")]
+    [Fact(Skip="f")]
     internal static void RegressionTest()
     {
         var expected = 42;
